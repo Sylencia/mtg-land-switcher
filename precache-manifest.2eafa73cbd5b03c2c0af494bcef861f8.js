b057fc50f2f774425d22e394b37612f4a4be98e4
Joseph Tran <joseph.tran@akqa.com>
2019-03-29 16:16:28 +1100

    Updates

---
+++ precache-manifest.2eafa73cbd5b03c2c0af494bcef861f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b42fdf61fa29939c14",
+    "url": "/mtg-land-switcher/static/js/runtime~main.2fb462f7.js"
+  },
+  {
+    "revision": "953a2f311d12168faa0a",
+    "url": "/mtg-land-switcher/static/js/main.44aee867.chunk.js"
+  },
+  {
+    "revision": "1d1af15c93faa7e61b00",
+    "url": "/mtg-land-switcher/static/js/2.6bbd2876.chunk.js"
+  },
+  {
+    "revision": "953a2f311d12168faa0a",
+    "url": "/mtg-land-switcher/static/css/main.1944d5a3.chunk.css"
+  },
+  {
+    "revision": "283dddce2b050e2e7522c2d471c8ac67",
+    "url": "/mtg-land-switcher/index.html"
+  }
+];
