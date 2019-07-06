020653b1530bf032e2e21838aadc10a7b5cb1636
Joseph Tran <joseph.tran.p@gmail.com>
2019-07-07 02:05:37 +1000

    Updates

---
+++ precache-manifest.a0c6df508b24352c33b8dec9c9d6e2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8b5cfacf2f2ed18f527e20fa29e2de",
+    "url": "/landcycler/index.html"
+  },
+  {
+    "revision": "debf78b1b93839714ff5",
+    "url": "/landcycler/static/css/main.d972b4d2.chunk.css"
+  },
+  {
+    "revision": "a917ca288af969b013c4",
+    "url": "/landcycler/static/js/2.eeca14c5.chunk.js"
+  },
+  {
+    "revision": "debf78b1b93839714ff5",
+    "url": "/landcycler/static/js/main.9589c32a.chunk.js"
+  },
+  {
+    "revision": "f753b7867b16cb34ad40",
+    "url": "/landcycler/static/js/runtime~main.53ae7d6b.js"
+  },
+  {
+    "revision": "824143804c5f4f892588914427f7088b",
+    "url": "/landcycler/static/media/settings.82414380.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/landcycler/precache-manifest.d41725db3243fe638480a676373f8403.js"
+  "/landcycler/precache-manifest.a0c6df508b24352c33b8dec9c9d6e2d7.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/landcycler/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/landcycler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
